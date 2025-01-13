d8f4b2d2272b812b95afa507055f68c4b65fd2e5
SerGio_Play <99011304+SerGioPlay01@users.noreply.github.com>
2025-01-13 23:22:59 +0300

    up

--- js/copy.js
+++ js/copy.js
@@ -1,18 +1,19 @@
 function copyCode() {
-  const success = document.querySelector(".copy-button");
-  const codeBlock = document.getElementsByTagName("code").textContent;
-  navigator.clipboard
-    .writeText(codeBlock)
-    .then(() => {
-      // Изменить текст кнопки на "Скопировано!"
-      success.textContent = "Скопировано!";
-
-      // Вернуть исходный текст кнопки через 2 секунды
-      setTimeout(() => {
-        success.textContent = "Скопировать";
-      }, 2000);
-    })
-    .catch((err) => {
-      console.error("Ошибка копирования:", err);
-    });
-}
+    const success = document.querySelector(".copy-button");
+    const codeBlock = document.querySelector("code").textContent; // Исправлено на querySelector
+    navigator.clipboard
+      .writeText(codeBlock)
+      .then(() => {
+        // Изменить текст кнопки на "Скопировано!"
+        success.textContent = "Скопировано!";
+  
+        // Вернуть исходный текст кнопки через 2 секунды
+        setTimeout(() => {
+          success.textContent = "Скопировать";
+        }, 2000);
+      })
+      .catch((err) => {
+        console.error("Ошибка копирования:", err);
+      });
+  }
+  
